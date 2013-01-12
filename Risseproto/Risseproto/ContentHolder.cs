36eb6c561e9a640325d5a2b069d001e4a92ed50a
Simen Løkken <simen.lokken@gmail.com>
2013-01-12 14:26:48 +0100

    Initial commit

---
+++ Risseproto/Risseproto/ContentHolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Risseproto
+{
+    class ContentHolder
+    {
+        public Texture2D risse;
+
+        public ContentHolder(ContentManager content)
+        {
+            loadTextures(content);
+            //loadSounds(content);
+            //loadParticles(content);
+            //loadFonts(content);
+        }
+
+        public void loadTextures(ContentManager content)
+        {
+            risse = content.Load<Texture2D>("risse");
+        }
+    }
+}
---
+++ Risseproto/Risseproto/Gameobject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Risseproto
+{
+    class Gameobject
+    {
+        private Vector2 position;
+        private Texture2D texture;
+
+        public Gameobject(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+            spriteBatch.Draw(texture, position, null, Color.White, 0f,
+                    position, 0.2f, SpriteEffects.None, 0f);
+        }
+    }
+}
