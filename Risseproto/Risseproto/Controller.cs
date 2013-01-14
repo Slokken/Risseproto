c3cac556f504d8d2b0801280cdfbb05c8755c4b1
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2013-01-14 02:39:22 +0100

    collision stuff

--- Risseproto/Risseproto/Controller.cs
+++ Risseproto/Risseproto/Controller.cs
@@ -22,6 +22,7 @@ namespace Risseproto
             risse = gameWorld.Risse;
             parallaxBackground(gameWorld);
             physicsEngine.gravitation(risse, gameTime);
+            collisionResolution(gameWorld);
 
         }
 
@@ -46,6 +47,74 @@ namespace Risseproto
             risse.Velocity = new Vector2(0, -10);
         }
 
+        protected void collisionResolution(Gameworld gameworld)
+        {
+            bool collidedWithPlatformSide = false;
+            foreach (Gameobject platform in gameworld.Platforms)
+            {
+                if (physicsEngine.collisionDetection(risse, platform))
+                {
+                    if (collisionDetermineType(gameworld, risse, platform))
+                    {
+                        collidedWithPlatformSide = true;
+                        break;
+                    }
+                }
+            }
 
+            if (!collidedWithPlatformSide)
+            {
+                foreach (Gameobject platform in gameworld.Ground)
+                {
+                    if (physicsEngine.collisionDetection(risse, platform))
+                    {
+                        if (collisionDetermineType(gameworld, risse, platform))
+                        {
+                            collidedWithPlatformSide = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!collidedWithPlatformSide)
+            {
+                foreach (Gameobject collidable in gameworld.Collidables)
+                {
+                    if (physicsEngine.collisionDetection(risse, collidable))
+                    {
+                        collisionHorizontal(gameworld);
+                    }
+                }
+            }
+        }
+
+        // returns true if colliding from side
+        protected bool collisionDetermineType(Gameworld gameworld, Gameobject risse, Gameobject platform)
+        {
+            if (risse.BoundingBox.Right - (risse.BoundingBox.Width/2) > platform.BoundingBox.Left && risse.BoundingBox.Right - (risse.BoundingBox.Width/2) < platform.BoundingBox.Right)
+            {
+                collisionHorizontal(gameworld);
+                return true;
+            }
+
+            collisionVertical(gameworld);
+            return false;
+        }
+
+        //handles crashing
+        protected void collisionHorizontal(Gameworld gameworld)
+        {
+            //gameworld.Risse.Velocity = Vector2.Zero;
+
+            //FUCK MY LIFE HE HAS TO FALL AND STOP
+        }
+
+        //handles landing on or jumping up and hitting a platform
+        protected void collisionVertical(Gameworld gameworld)
+        {
+            gameworld.Risse.Position = new Vector2(gameworld.Risse.Position.X, gameworld.Risse.Position.Y - gameworld.Risse.Velocity.Y);
+            gameworld.Risse.Velocity = new Vector2(gameworld.Risse.Velocity.X, 0);
+        }
     }
 }
